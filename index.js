468945812927707ebdf43544adc2004252874bd5
Reese Garth <gart6270@mylaurier.ca>
2014-07-05 03:30:08 +0000

    Pulled text from index.html

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var buff = fs.readFileSync("index.html");
+var message = buff.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(message)
 })
 
 app.listen(app.get('port'), function() {
