6dfd2670530ea6ee889e69eb366f6fa3a235758a
Reese Garth <gart6270@mylaurier.ca>
2014-07-05 03:36:18 +0000

    Pulled text from index.html

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-var buff = fs.readFileSync("index.html");
+var buff = new Buffer(16);
+buff = fs.readFileSync("index.html");
 var message = buff.toString();
 
 app.get('/', function(request, response) {
