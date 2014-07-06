0e724aa2a0a73b7b08c879b9d4ddf373eb248796
Reese Garth <gart6270@mylaurier.ca>
2014-07-06 04:03:33 +0000

    Changed index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var express = require('express')
+var express = require('express');
+var fs = require('fs');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 var buff = new Buffer(32);
 buff = fs.readFileSync("index.html");
 var message = buff.toString('utf-8');
-console.log(message);
 
 app.get('/', function(request, response) {
-  response.send(message)
-})
+  response.send(message);
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
