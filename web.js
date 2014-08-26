be0ed9dddeda56dfc26fd6c3279e1cac3fe1793a
ReeseGarth <gart6270@mylaurier.ca>
2014-08-26 22:46:59 +0000

    edited product desc in index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
+#!/usr/bin/env node
 var express = require('express');
+var morgan = require('morgan')
 
-var app = express.createServer(express.logger());
+var app = express()
+app.use(morgan('combined'))
 
 app.get('/', function(request, response) {
     response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
